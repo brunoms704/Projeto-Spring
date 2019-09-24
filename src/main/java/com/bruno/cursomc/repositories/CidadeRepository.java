dfba9436be22a343adac818b0de23d8aec2b014e
Bruno Machado <brunoms704@gmail.com>
2019-09-24 14:22:47 -0300

    Estado e cidade

--- src/main/java/com/bruno/cursomc/repositories/CidadeRepository.java
+++ src/main/java/com/bruno/cursomc/repositories/CidadeRepository.java
@@ -3,9 +3,9 @@ package com.bruno.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bruno.cursomc.domain.Produto;
+import com.bruno.cursomc.domain.Cidade;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
 
 }
--- src/main/java/com/bruno/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/bruno/cursomc/repositories/EstadoRepository.java
@@ -3,9 +3,9 @@ package com.bruno.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bruno.cursomc.domain.Produto;
+import com.bruno.cursomc.domain.Estado;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
 
 }
