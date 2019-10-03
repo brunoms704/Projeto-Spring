befb5d8c39e9d6ce077bb82b053b3f250a65d982
Bruno Machado <brunoms704@gmail.com>
2019-10-03 16:30:36 -0300

    Salvar atualizacoes

---
+++ src/main/java/com/bruno/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,27 @@
+package com.bruno.cursomc.domain;
+
+import java.io.Serializable;
+
+public class PagamentoComCartao extends Pagamento implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer numeroDeParcelas;
+
+	public PagamentoComCartao() {
+
+	}
+
+	public PagamentoComCartao(Integer numeroDeParcelas) {
+		super();
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+}
---
+++ src/main/java/com/bruno/cursomc/domain/Pedido.java
@@ -0,0 +1,75 @@
+package com.bruno.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Pedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Date instante;
+
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+
+	private Endereco enderecoDeEntrega;
+	
+	@ManyToMany
+	private List<Produto> itens = new ArrayList<>();
+
+	@OneToOne
+	private Pagamento pagamento;
+
+	public Pedido() {
+
+	}
+
+	public Pedido(Integer id, Date instante, Cliente cliente, Pagamento pagamento) {
+		super();
+		this.id = id;
+		this.instante = instante;
+		this.cliente = cliente;
+		this.pagamento = pagamento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
