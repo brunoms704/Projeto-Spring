863bfc76bc4d0d9a68270b55da0282732d67aa85
Bruno Machado <brunoms704@gmail.com>
2019-10-03 14:32:51 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/bruno/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.bruno.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bruno.cursomc.domain.Cliente;
+import com.bruno.cursomc.repositories.ClienteRepository;
+import com.bruno.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	public Cliente find(Integer id) throws ObjectNotFoundException {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getSimpleName()));
+
+	}
+
+}
